be974e6ee471fd34129dd7f0a727913690ff73a5
Armel Pingault <armel.pingault@netcentric.biz>
2018-12-04 21:33:02 +0100

    Update cache

--- README.md
+++ README.md
@@ -5,11 +5,3 @@ Deploy locally
 cd ./docs
 bundle exec jekyll serve
 ```
-
-Generate the ServiceWorker sw.js
-```
-cd ./
-create an empty index.html: ./docs/index.html
-npm run build
-delete the empty index.html
-```
--- docs/assets/scripts/main.js
+++ docs/assets/scripts/main.js
@@ -1,15 +1,3 @@
-if ('serviceWorker' in navigator) {
-  // Register a service worker hosted at the root of the
-  // site using a more restrictive scope.
-  navigator.serviceWorker.register('/sw.js', {scope: '/'}).then(function(registration) {
-    console.log('Service worker registration succeeded:', registration);
-  }, /*catch*/ function(error) {
-    console.log('Service worker registration failed:', error);
-  });
-} else {
-  console.log('Service workers are not supported.');
-}
-
 var newXHR = new XMLHttpRequest();
 newXHR.addEventListener( 'load', reqListener );
 newXHR.open( 'GET', 'https://www.googleapis.com/pagespeedonline/v2/runPagespeed?url=https%3A%2F%2Fwww.armelpingault.com&key=AIzaSyDZkYecmL2TehrGSVRlhxnSehuSpMoWji8' );
