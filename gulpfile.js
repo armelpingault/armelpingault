b300bfad23b02eeddf3b6e278f796aae8821362c
Armel Pingault <armel.pingault@netcentric.biz>
2018-12-04 19:34:29 +0100

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,10 @@
 var gulp = require('gulp');
 
-// gulp.task('default', ['generate-service-worker']);
-
-// function defaultTask(done) {
-//     // place code for your default task here
-//     done();
-// }
-
 gulp.task('generate-service-worker', function(callback) {
     var swPrecache = require('sw-precache');
     var rootDir = './docs';
 
-    swPrecache.write(`${rootDir}/service-worker.js`, {
+    swPrecache.write(`${rootDir}/sw.js`, {
         staticFileGlobs: [
             rootDir + '/*.{html,js,json,xml}',
             rootDir + '/assets/**/*.{js,css,png,jpg,gif,svg,eot,ttf,woff}'
